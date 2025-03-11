03d5645a5edfe06efd5e8f39ebdefcaf574c2687
valid <85952515+OHOKs@users.noreply.github.com>
2025-03-11 21:23:22 +0100

    Update listPhones.xaml.cs

--- C#/entity-framework-mysql/listPhones.xaml.cs
+++ C#/entity-framework-mysql/listPhones.xaml.cs
@@ -55,3 +55,86 @@ namespace Telok
         }
     }
 }
+
+
+
+using MySql.Data.MySqlClient;
+using System;
+using System.Windows;
+using System.Windows.Controls;
+
+namespace Telok
+{
+    public partial class listPhones : Window
+    {
+        private string ConnectionString = "server=localhost;uid=root;database=telefonokdb";
+        private MySqlConnection connection;
+        private MySqlCommand command;
+
+        public listPhones()
+        {
+            InitializeComponent();
+            connection = new MySqlConnection(ConnectionString);
+            LoadPhones();
+        }
+
+        private void LoadPhones()
+        {
+            telok.Items.Clear();
+            try
+            {
+                connection.Open();
+                command = connection.CreateCommand();
+                command.CommandText = "SELECT Sorszam, Nev, Marke FROM telefonok";
+                MySqlDataReader reader = command.ExecuteReader();
+                while (reader.Read())
+                {
+                    ListBoxItem item = new ListBoxItem
+                    {
+                        Content = reader.GetString("Nev") + " - " + reader.GetString("Marke"),
+                        Tag = reader.GetInt32("Sorszam")
+                    };
+                    telok.Items.Add(item);
+                }
+                connection.Close();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Hiba történt az adatok betöltésekor: " + ex.Message);
+            }
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            if (telok.SelectedItem is ListBoxItem selectedItem)
+            {
+                int sorszam = (int)selectedItem.Tag;
+                DeletePhone(sorszam);
+            }
+        }
+
+        private void DeletePhone(int sorszam)
+        {
+            try
+            {
+                connection.Open();
+                command = connection.CreateCommand();
+                command.CommandText = "DELETE FROM telefonok WHERE Sorszam = @sorszam";
+                command.Parameters.AddWithValue("@sorszam", sorszam);
+                command.ExecuteNonQuery();
+                connection.Close();
+                LoadPhones();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Hiba történt a törlés során: " + ex.Message);
+            }
+        }
+
+        private void Window_Closing(object sender, System.ComponentModel.CancelEventArgs e)
+        {
+            var newpage = new MainWindow();
+            newpage.Show();
+        }
+    }
+}
